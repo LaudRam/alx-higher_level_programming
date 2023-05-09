2819a52312bfcb4b4585931af03fe0b57865fe93
LaudRam <Laudiciaramasenya@gmail.com>
2023-05-09 19:19:05 +0000

    Advanced tasks commit

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def remove_char_at(str, n):
+
+    i = str[: n]        #store characters before nth position
+    j = str[n + 1:]     #store characters after nth position
+    return (i + j)
