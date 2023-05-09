195f7680b55d510c8ca33625fa3712577a35ab93
LaudRam <Laudiciaramasenya@gmail.com>
2023-05-09 19:22:33 +0000

    Updated task 15

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -2,6 +2,6 @@
 
 def remove_char_at(str, n):
 
-    i = str[: n]        #store characters before nth position
-    j = str[n + 1:]     #store characters after nth position
+    i = str[: n]        # store characters before nth position
+    j = str[n + 1:]     # store characters after nth position
     return (i + j)
