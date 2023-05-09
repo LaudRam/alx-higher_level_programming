d1edb7159678616fac15abb847dc492436f4c0e8
LaudRam <Laudiciaramasenya@gmail.com>
2023-05-09 19:26:35 +0000

    Fixed error

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 
 for i in range(122, 96, -1):
-    print(chr(i) if i % 2 == 0 else chr(i - 32), end="")
+    if i % 2 != 0:
+        i = i - 32
+    print("{}".format(chr(i)), end="")
