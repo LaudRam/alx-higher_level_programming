ca7b3876e9b169ce8b88c8e2c800997259215e9e
LaudRam <Laudiciaramasenya@gmail.com>
2023-05-15 15:22:49 +0000

    Initial commit

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,19 @@
+#include <Python.h>
+
+/**
+ * print_python_list_info - prints basic info about Python lists
+ * @p: python object
+ */
+
+void print_python_list_info(PyObject *p)
+{
+	int i;
+	long int size = PyList_Size(p);
+	PyListObject *obj = (PyListObject *)p;
+
+	printf("[*] Size of Python List = %li\n", size);
+	printf("[*] Allocated = %li\n", obj->allocated);
+
+	for (i = 0; i < size; i++)
+		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
+}
