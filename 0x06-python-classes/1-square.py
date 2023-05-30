740240de780b04e3ff5cbf354bf240048ffb9bb1
LaudRam <laudiciaramasenya@gmail.com>
2023-05-30 10:07:21 +0000

    Task 1 commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+''' Square module '''
+
+
+class Square():
+    ''' Defines a square '''
+
+    def __init__(self, size):
+        ''' Initialisation of instance attributes
+            Args - size: Size of the square
+        '''
+        self.__size = size
