5c7733a5734e824e4e218fd2bfe3ba181605454b
LaudRam <laudiciaramasenya@gmail.com>
2023-05-30 20:40:05 +0000

    Task 6 update

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -17,10 +17,6 @@ class Square():
                 TypeError: if size is not an integer
                 ValueError: if size < 0
         '''
-        if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
         self.__size = size
         self.__position = position
 
