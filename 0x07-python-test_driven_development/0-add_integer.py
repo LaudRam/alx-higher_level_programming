2679fb8fda5c034e4b9d369a11cce8b6d0511a26
LaudRam <laudiciaramasenya@gmail.com>
2023-06-03 11:11:49 +0200

    task 0 commit

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+
+''' Module for adding integers '''
+
+
+def add_integer(a, b=98):
+    ''' Adds two integers
+
+        Raises - TypeError: if either a or b is not an integer or a float
+    '''
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError('a must be an integer')
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError('b must be an integer')
+    return (int(a + b))
