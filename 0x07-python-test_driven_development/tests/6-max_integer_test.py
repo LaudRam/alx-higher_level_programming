25f079c2379f810b305d5a6538663ad393c0c9bf
LaudRam <laudiciaramasenya@gmail.com>
2023-06-04 19:30:55 +0200

    test 6 updated

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -33,9 +33,25 @@ class TestMaxInteger(unittest.TestCase):
         ''' Tests for negative values '''
         self.assertEqual(max_integer([-1, -69, -420, -2]), -69)
 
+    def test_floats(self):
+        ''' Tests for floating point numbers '''
+        self.assertEqual(max_integer([4.544, -9.8, 8.3, 3.3333]), 8.3)
+
     def test_one(self):
         ''' Tests for list with only one parameter '''
         self.assertEqual(max_integer([13]), 13)
+
+    def test_beginning(self):
+        ''' Tests for when max value is at the beginning '''
+        self.assertEqual(max_integer([80, 6, 6, 6]), 80)
+
+    def test_ordered(self):
+        ''' Tests for ordered list of integers '''
+        self.assertEqual(max_integer([2, 4, 6, 8]), 8)
+
+    def test_unordered(self):
+        ''' Tests for unordered list of integers '''
+        self.assertEqual(max_integer([6, 8, 2, 4]), 8)
 
     def test_correct(self):
         ''' Tests for passing an argument that's expected to succeed '''
