5afcb1c9d69f05b32359963ec32f6809916f075f
LaudRam <laudiciaramasenya@gmail.com>
2023-06-04 13:49:41 +0200

    Task 3 commit

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+
+''' Module for printing name '''
+
+
+def say_my_name(first_name, last_name=""):
+    ''' Prints a name
+
+        Args:
+            first_name (str): first name
+            last_name (str): last name
+        Raises:
+            TypeError: if either first_name or last_name is not a string
+    '''
+    if not isinstance(first_name, str):
+        raise TypeError('first_name must be a string')
+    if not isinstance(last_name, str):
+        raise TypeError('last_name must be a string')
+    print('My name is {} {}'.format(first_name, last_name))
