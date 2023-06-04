f654e069b0388615a59e556b5716a2274fd7b96a
LaudRam <laudiciaramasenya@gmail.com>
2023-06-04 14:27:11 +0200

    Task 5 commit

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+
+''' Printing module '''
+
+
+def text_indentation(text):
+    ''' Prints text with 2 new lines after each of these characters:
+        '.', '?' and ':'
+
+        Args:
+            text (str): text to print
+
+        Raise:
+            TypeError: if text is not a string
+    '''
+    if not isinstance(text, str):
+        raise TypeError('text must be a string')
+
+    lett = 0
+    while lett < len(text) and text[lett]:
+        lett += 1
+
+    while lett < len(text):
+        print(text[lett], end='')
+        if text[lett] == '\n' or text[lett] in '.?:':
+            if text[lett] in '.?:':
+                print('\n')
+            lett += 1
+            while lett < len(text) and text[lett] == ' ':
+                lett += 1
+            continue
+        lett += 1
