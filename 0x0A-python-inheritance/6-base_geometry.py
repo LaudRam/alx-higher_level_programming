bdae9e1eaaf8e61466ae1a6970ca636a20ab4712
LaudRam <laudiciaramasenya@gmail.com>
2023-06-12 11:08:07 +0200

    Task 6 commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+''' BaseGeometry module '''
+
+
+class BaseGeometry():
+    ''' Class based on 5-base_geometry.py '''
+    def area(self):
+        ''' Raises exception when called '''
+        raise Exception('area() is not implemented')
