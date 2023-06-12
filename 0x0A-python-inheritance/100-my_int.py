2342f143e4ba596d4d53d68dfe2b5ba92aaab7a8
LaudRam <laudiciaramasenya@gmail.com>
2023-06-12 12:17:16 +0200

    Task 100 modified

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -7,13 +7,13 @@ class MyInt(int):
     def __init__(self, num):
         self.num = num
 
-    def _eq_(self, value):
-        ''' Overide == with != behavior '''
-        return self.num != value
-
     def _ne_(self, value):
         ''' Overide != with == behavior '''
         return self.num == value
+
+    def _eq_(self, value):
+        ''' Overide == with != behavior '''
+        return self.num != value
 
     def __str__(self):
         return str(self.num)
