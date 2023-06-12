985909dbff16a160d87bc9ace13a11b8e09d89cc
LaudRam <laudiciaramasenya@gmail.com>
2023-06-12 11:34:10 +0200

    Task 8 commit

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+''' Class Rectangle that inherits BaseGeometry '''
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    ''' Defines rectangle using BaseGeometry '''
+    def __init__(self, width, height):
+        ''' Initialises new rectangle
+
+            Args:
+                width (int): width of rectangle
+                height (int): height of rectangle
+        '''
+        self.integer_validator('width', width)
+        self.__width = width
+        self.integer_validator('height', height)
+        self.__height = height
