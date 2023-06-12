22ddcf36ca5376c4e360456650b8320d1eab0770
LaudRam <laudiciaramasenya@gmail.com>
2023-06-12 12:14:53 +0200

    Task 100 commit

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -4,10 +4,16 @@
 
 class MyInt(int):
     ''' Inverts int operators == and != '''
+    def __init__(self, num):
+        self.num = num
+
     def _eq_(self, value):
         ''' Overide == with != behavior '''
-        return self.real != value
+        return self.num != value
 
     def _ne_(self, value):
         ''' Overide != with == behavior '''
-        return self.real == value
+        return self.num == value
+
+    def __str__(self):
+        return str(self.num)
